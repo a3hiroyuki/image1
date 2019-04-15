40dcbe8f4c69676efe5eaa7fc900bf0def662fab
hiro <abe@gmail.com>
2019-04-16 01:57:02 +0900

    ten

--- gui_test2.py
+++ gui_test2.py
@@ -2,9 +2,10 @@ import math
 
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseEvent
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
-import time, threading
-
+import tkinter
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 CUR_DIR = 'C:\\aaa\\'
 
@@ -19,9 +20,7 @@ class PlotPoints():
         angles = df['angle'].values.astype('float')
         for i in range(len(no_arr)):
             self.points[int(no_arr[i])] = PlotPoint(x_arr[i], y_arr[i], angles[i])
-
-    def items(self):
-        return [ (point.x, point.y) for point in self.points.values()]
+        self.check_in_circle()
 
     def add_point(self, x, y=0):
         if isinstance(x, MouseEvent):
@@ -30,7 +29,8 @@ class PlotPoints():
         self.no = self.no + 1
 
     def remove_point(self, point_no):
-        self.points.pop(point_no)
+        #self.points.pop(point_no)
+        self.points[point_no].is_del = True
 
     def get_nearest_point(self, event):
         distance_threshold = 3.0
@@ -45,38 +45,58 @@ class PlotPoints():
             return nearest_point_no
         return None
 
+    def check_in_circle(self):
+        radius = 10
+        r2 = radius * radius
+        for point1 in self.points.values():
+            for point2 in self.points.values():
+                if (not point1 == point2) and (not point1.is_del) and (not point2.is_del):
+                    point_val = (point2.x - point1.x) * (point2.x - point1.x) + (point2.y - point1.y) * (point2.y - point1.y)
+                    if point_val <= r2:
+                        point2.is_del = True
+
+
 class PlotPoint():
+
     def __init__(self, x, y, angle):
         self.x = x
         self.y = y
         self.angle = angle
+        self.is_del = False
 
-class DraggablePlotExample(object):
-    u""" An example of plot with draggable markers """
+class DraggablePlot(object):
 
     def __init__(self):
         self._figure, self._axes, self._line = None, None, None
         self._dragging_point = None
         self.plot_points = PlotPoints()
-
         self._init_plot()
 
     def _init_plot(self):
-
-
         self._figure = plt.figure("Example plot")
         axes = plt.subplot(1, 1, 1)
         axes.set_xlim(0, 100)
         axes.set_ylim(0, 100)
         axes.grid(which="both")
         self._axes = axes
-
         self._figure.canvas.mpl_connect('button_press_event', self._on_click)
         #self._figure.canvas.mpl_connect('button_release_event', self._on_release)
         #self._figure.canvas.mpl_connect('motion_notify_event', self._on_motion)
-
-        self._update_plot()
+        axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
+        bnext = Button(axnext, 'delete')
+        #bnext.on_clicked(on_button_clicked)
+        #self._figure.canvas.mpl_connect('button_press_event', on_press)
+        self.update_plot()
         plt.show()
+
+    def update_plot(self):
+        for point in self.plot_points.points.values():
+            if point.is_del:
+                self._axes.scatter(point.x,point.y, c='red')
+            else:
+                self._axes.scatter(point.x,point.y, c='blue')
+        self._figure.canvas.draw()
+
 
 
     def _on_click(self, event):
@@ -88,44 +108,15 @@ class DraggablePlotExample(object):
                 self.plot_points.remove_point(point_no)
             else:
                 self.plot_points.add_point(event)
-            self._update_plot()
+            self.update_plot()
         # right click
         elif event.button == 3 and event.inaxes in [self._axes]:
             point_no = self.plot_points.get_nearest_point(event)
             if point_no:
                 self.plot_points.remove_point(point_no)
-                self._update_plot()
-
-    def _update_plot(self):
-        xy = self.plot_points.items()
-        x, y = map(list, zip(*xy))
-        if not self._line:
-            self._line, = self._axes.plot(x, y, "b", marker="o", markersize=10)
-        else:
-            self._line.set_data(x, y)
-        self._figure.canvas.draw()
-
-#     def _on_release(self, event):
-#         u""" callback method for mouse release event
-#
-#         :type event: MouseEvent
-#         """
-#         if event.button == 1 and event.inaxes in [self._axes] and self._dragging_point:
-#             self._add_point(event)
-#             self._dragging_point = None
-#             self._update_plot()
-#
-#     def _on_motion(self, event):
-#         u""" callback method for mouse motion event
-#
-#         :type event: MouseEvent
-#         """
-#         if not self._dragging_point:
-#             return
-#         self._remove_point(*self._dragging_point)
-#         self._dragging_point = self._add_point(event)
-#         self._update_plot()
-
+                self.update_plot()
 
 if __name__ == "__main__":
-    plot = DraggablePlotExample()
+    plot = DraggablePlot()
+
+
